d6e902e91898eec1d8612b36759a012f38926ee9
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2021-12-15 15:27:17 +0530

    Created GameOverController.cs along with Game Over Text and Restart button

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverController : MonoBehaviour
+{
+
+    //Player dies
+    public void PlayerDied()
+    {
+        gameObject.SetActive(true);
+    }
+}
