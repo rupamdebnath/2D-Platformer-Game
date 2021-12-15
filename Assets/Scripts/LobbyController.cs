002396aa1fa730c66c142bf04c5d353141a7df48
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2021-12-15 17:40:06 +0530

    Added Lobby Scene with Game title Play button, on clicking the button, player starts level 1 scene.

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+
+    public Button buttonPlay;
+    // Awake
+    void Awake()
+    {
+        buttonPlay.onClick.AddListener(PlayGame);
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
